82c81107ca4233eb7adfb9d4749e3244220b7425
CKHope <chungkien20211011@gmail.com>
2023-07-28 00:56:05 +0700

    ud

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ dfDictData=dfDictData[['character','one','radical']]
 dfbothu=pd.read_pickle('05bothu.pkl')
 
 def main():
+    st.balloons()
     st.title("Chinese Character Radicals Finder")
 
     paragraph = st.text_area("Enter a paragraph in Chinese:", height=200)
@@ -27,19 +28,23 @@ def main():
     temp_df = pd.DataFrame({'radical': listDecomposition})
     temp_df['temp_index'] = range(len(temp_df))
     dfRadical=lookup_and_copy_values(dfMain=temp_df,dfMainColumn='radical',dfSub=dfbothu,dfSubColumn='radical')
-    
+    col=st.columns([1,1])
     # st.write(*listDecomposition)
-    if len(dfRadical)>0:
-        st.success(f"Radical to learn: {len(dfRadical)}")
-        with st.expander("Start to learn"):
-            dfRadical=dfRadical[['radical_y','name','pinyin','meaning','stroke']]
-            dfRadical=dfRadical.dropna()
-            dfRadical=dfRadical.reset_index()
-            dfRadical=dfRadical[['radical_y','name','pinyin','meaning','stroke']]
-            st.dataframe(dfRadical)
-    if len(dfDecomposition)>0:  
-        with st.expander("See full database"):
-            dfDecomposition
-
+    with col[0]:
+        if len(dfRadical)>0:
+            st.success(f"Radical to learn: {len(dfRadical)}")
+            with st.expander("Start to learn"):
+                dfRadical=dfRadical[['radical_y','name','pinyin','meaning','stroke']]
+                dfRadical=dfRadical.dropna()
+                dfRadical=dfRadical.reset_index()
+                dfRadical.drop('index', axis=1, inplace=True)
+                dfRadical
+    with col[1]:            
+        if len(dfDecomposition)>0:
+            st.success(f"Unique words: {len(dfDecomposition)}")  
+            with st.expander("See full database"):
+                dfDecomposition=dfDecomposition.reset_index()
+                dfDecomposition.drop('index', axis=1, inplace=True)
+                dfDecomposition
 if __name__ == "__main__":
     main()
